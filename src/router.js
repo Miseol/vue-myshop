0e84d39909eb2b652c6e3f024c214f89922c6d19
Miseol <miseo@foxmail.com>
2020-04-12 03:19:37 +0800

    power over~

--- src/router.js
+++ src/router.js
@@ -6,12 +6,6 @@ import Welcome from './components/Welcome.vue'
 import Users from './components/user/Users.vue'
 import Rights from './components/power/Rights.vue'
 import Roles from './components/power/Roles.vue'
-import Cate from './components/goods/Cate.vue'
-import Params from './components/goods/Params.vue'
-import GoodsList from './components/goods/List.vue'
-import Add from './components/goods/Add.vue'
-import Order from './components/order/Order.vue'
-import Report from './components/report/Report.vue'
 
 Vue.use(Router)
 
@@ -27,13 +21,7 @@ const router = new Router({
         { path: '/welcome', component: Welcome },
         { path: '/users', component: Users },
         { path: '/rights', component: Rights },
-        { path: '/roles', component: Roles },
-        { path: '/categories', component: Cate },
-        { path: '/params', component: Params },
-        { path: '/goods', component: GoodsList },
-        { path: '/goods/add', component: Add },
-        { path: '/orders', component: Order },
-        { path: '/reports', component: Report }
+        { path: '/roles', component: Roles }
       ]
     }
   ]
